0c79a36a5e13f64fec8471106b62b3c4a30da5ef
Melissa Benard <benardmeli@yahoo.fr>
2024-04-23 08:36:35 +0200

    Display the hour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './article/article.component';
+import { HourComponent } from './hour/hour.component';
+import { LogsComponent } from './logs/logs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticleComponent,
+    HourComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,
