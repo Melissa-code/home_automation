3e30e7bcdb866ff9a2ffcbcde89e72edc34b91c8
Océane Princep <94532496+oce-prcp@users.noreply.github.com>
2024-04-23 11:59:58 +0200

    update

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './article.component.css'
 })
 
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
 
   roomsList: { [key: string]: boolean } = {
     "Salle à manger": false, 
@@ -22,13 +22,36 @@ export class ArticleComponent {
   roomStatus: boolean[] = Array(this.roomsList.length).fill(false);
   statusRoom: boolean = false; 
   iconActive: boolean = false; 
+  
+  constructor() {
+    // Définir une vérification initiale au chargement du composant
+    this.checkTimeAndTurnOffLights();
+  }
 
+  ngOnInit() {
+    // Vérifier périodiquement (toutes les minutes)
+    setInterval(() => {
+      this.checkTimeAndTurnOffLights();
+    }, 60000);
+  }
+  
+  checkTimeAndTurnOffLights() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+    if (currentHour >= 23) {
+      this.roomsList['Piscine'] = false;  // Éteindre la lumière de la piscine après 23h
+    }
+  }
+  
   /**
    * Change the color of the icons of the rooms : green or red
    */
   toggleColor(room: string) {
-    this.roomsList[room] = !this.roomsList[room];
+    if (!(room === 'Piscine' && new Date().getHours() >= 23)) { // Ne pas allumer la piscine après 23h
+      this.roomsList[room] = !this.roomsList[room];
+    }
   }
+
   
   getObjectKeys(obj: any): string[] {
     return Object.keys(obj);
