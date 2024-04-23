d85090efea4fd16ac65df9768fa008038e77acb7
Melissa Benard <benardmeli@yahoo.fr>
2024-04-23 13:00:21 +0200

    Create a hour service to have currentHour

--- src/app/hour/hour.component.ts
+++ src/app/hour/hour.component.ts
@@ -1,33 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HourService } from '../hour.service';
 
 @Component({
   selector: 'app-hour',
   templateUrl: './hour.component.html',
-  styleUrl: './hour.component.css'
+  styleUrls: ['./hour.component.css']
 })
+export class HourComponent implements OnInit {
+  currentHour: string;
 
-export class HourComponent {
-  currentDate: Date;
-  currentHour: string; 
-
-  constructor() {
-    this.currentDate = new Date();
-    this.currentHour = `${this.currentDate.getHours()}:${this.currentDate.getMinutes()}`; 
+  constructor(private hourService: HourService) {
+    this.currentHour = this.hourService.currentHour;
   }
 
-  /**
-   * Add 30 minutes to current hour 
-   */
+  ngOnInit(): void {
+  }
+
   addFortyMinutes() {
-    console.log(this.currentHour);
-    // convert hours in minutes and cast string to number
-    let currentHourInMinutes: number = parseInt(this.currentHour.split(':')[0]) * 60 + parseInt(this.currentHour.split(':')[1]);
-    let newHourInMinutes: number = currentHourInMinutes + 30;
-    newHourInMinutes = newHourInMinutes % (24 * 60);
-    let newHours: number = Math.floor(newHourInMinutes / 60);
-    let newMinutes: number = newHourInMinutes % 60; 
-    let paddedMinutes: string = newMinutes.toString().padStart(2, '0');
-    this.currentHour = `${newHours}:${paddedMinutes}`;
-    console.log('heure plus 30min',this.currentHour);
+    this.hourService.addFortyMinutes();
+    this.currentHour = this.hourService.currentHour;
   }
 }
--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HourService } from '../hour.service';
 
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
-  styleUrl: './logs.component.css'
+  styleUrls: ['./logs.component.css']
 })
-export class LogsComponent {
+export class LogsComponent implements OnInit {
+  currentHour: string;
+  currentDate: Date;
+
+  constructor(private hourService: HourService) {
+    this.currentHour = this.hourService.currentHour;
+    this.currentDate = this.hourService.currentDate;
+  }
+
+  ngOnInit(): void {
+    console.log("heure a afficher ici", this.currentHour); 
+    console.log(this.currentDate.getHours())
+  }
 
 }
