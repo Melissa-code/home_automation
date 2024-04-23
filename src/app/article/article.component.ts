73a0c8087876b0f1732a5fc9945616f22039f58d
Melissa Benard <benardmeli@yahoo.fr>
2024-04-23 10:37:05 +0200

    Change the icon status of the room with the button

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -8,28 +8,29 @@ import { Component } from '@angular/core';
 
 export class ArticleComponent {
 
-  roomsList: string[] = ["Salle à manger", "Salle de bain", "Garage", "PAC", "Buanderie", "Piscine"]; 
-  roomsListNumber: number = this.roomsList.length;  
+  roomsList: { [key: string]: boolean } = {
+    "Salle à manger": false, 
+    "Salle de bain": false, 
+    "Garage": false, 
+    "PAC": false, 
+    "Buanderie": false, 
+    "Piscine": false
+  };
+
+  roomsListNumber: number = Object.keys(this.roomsList).length;
   btnForce: string = "btn btn-primary"; 
   roomStatus: boolean[] = Array(this.roomsList.length).fill(false);
   statusRoom: boolean = false; 
   iconActive: boolean = false; 
 
   /**
-   * Light up the icons of the rooms 
+   * Change the color of the icons of the rooms : green or red
    */
-  // lightUp(index: number) {
-  //   console.log(this.roomStatus[index]);
-  //   this.roomStatus[index] = false; 
-  // }
-  toggleColor(index: number) {
-    this.iconActive = !this.iconActive; // Inverser l'état de l'icône
+  toggleColor(room: string) {
+    this.roomsList[room] = !this.roomsList[room];
   }
-
-  /**
-  * Force to change the status
-  */
-  toggleIconColor() {
-    this.iconActive = !this.iconActive; // Inverser l'état de l'icône
+  
+  getObjectKeys(obj: any): string[] {
+    return Object.keys(obj);
   }
 }
